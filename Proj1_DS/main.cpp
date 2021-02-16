85e6261cce772c1c1d10b606d7f2af73a49a5196
noahschlick <49171976+noahschlick@users.noreply.github.com>
2021-02-16 15:30:52 -0600

    Initial Commit

---
+++ Proj1_DS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Proj1_DS
+//
+//  Created by Noah Schlickeisen on 2/16/21.
+//  Copyright © 2021 Noah Schlickeisen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
